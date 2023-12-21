c9bf471ded7f13721363e1e25cb0ad71e4ccf0a6
Yousra <nice.sora@hotmail.com>
2023-12-21 20:33:37 +1100

    Added JavaScript logic/ updated css & html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,74 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//Wrapped all code in .ready() method to execute code when the HTML document is fully loaded.
+$(document).ready(function () {
+  // Variables selecting elements by id and class
+  var currentDaySection = $("#currentDay");
+  var mainContentSection = $(".container");
+  var workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+  /* Function to format and display current date and time
+  in the header of the page */
+  function dateTimeDisplay() {
+    //current date and time format
+    var currentDay = moment().format("dddd, MMMM D, YYYY HH:mm:ss A");
+    currentDaySection.text(currentDay);
+  }
+
+  // Function displays the time-blocks in the container
+  function displayTimeBlocks() {
+    for (var i = 0; i < workingHours.length; i++) {
+      //creating the row for each time-block
+      var row = $("<div>");
+      row.addClass("row time-block");
+
+      //creating the first column (workingHours column)
+      var hourcolumn = $("<div>");
+      hourcolumn.addClass("col-2 col-md-1 hour text-center py-3");
+      hourcolumn.text(moment().set("hour", workingHours[i]).format("h A"));
+
+      //creating the second column for the text
+      var textColumn = $("<textarea>");
+      textColumn.addClass("col-8 col-md-10 description");
+      textColumn.data("workday-hour", workingHours[i]);
+
+      /*  apply the past, present, or future class to each time block
+      based on the current hour */
+      if (moment().hour() > workingHours[i]) {
+        textColumn.addClass("past");
+      } else if (moment().hour() < workingHours[i]) {
+        textColumn.addClass("future");
+      } else {
+        textColumn.addClass("present");
+      }
+
+      //Set the text to localStorage saved value
+      if (localStorage.getItem(workingHours[i]) !== null) {
+        var savedText = localStorage.getItem(workingHours[i]);
+        textColumn.text(savedText);
+      }
+      //creating the third column for save button
+      var saveBtn = $("<button>");
+      saveBtn.addClass("btn saveBtn col-2 col-md-1 fas fa-save");
+
+      //Appending columns inside the row
+      row.append(hourcolumn);
+      row.append(textColumn);
+      row.append(saveBtn);
+      //Appending rows in main container
+      mainContentSection.append(row);
+    }
+  }
+
+  // Function to save user input to localStorage
+  function saveInput() {
+    var inputText = $(this.previousElementSibling).val();
+    var inputHour = $(this.previousElementSibling).data("workday-hour");
+    localStorage.setItem(inputHour, inputText);
+  }
+
+  dateTimeDisplay();
+  setInterval(dateTimeDisplay, 1000);
+  displayTimeBlocks();
+
+  // Event listener for click events on the save button
+  mainContentSection.on("click", "button", saveInput);
 });
